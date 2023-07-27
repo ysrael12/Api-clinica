5af8e1d3d1050febdf9d7275e63fba3ea3456021
Ysrael Sacrati <85563735+ysrael12@users.noreply.github.com>
2023-07-27 10:58:22 -0300

    Update main.py
    metodos adicionados

--- api/main.py
+++ api/main.py
@@ -70,7 +70,7 @@ def create_access_token(data, expires_delta):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# Rotas
+# Security // auth
 @app.post("/login")
 async def login(username: str, password: str):
     user = authenticate_user(username, password)
@@ -81,6 +81,17 @@ async def login(username: str, password: str):
     access_token = create_access_token({"sub": user["username"], "role": user["role"]}, access_token_expires)
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/users/", response_model=Usuario)
+async def create_user(user: Usuario):
+    hashed_password = get_password_hash(user.password)
+    query = Usuario.insert().values(
+        username=user.username,
+        password=hashed_password,
+        role=user.role
+    )
+    last_record_id = await database.execute(query)
+    return {**user.dict(), "id": last_record_id}
 
 
 @app.get("/protected")
@@ -106,14 +117,37 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+
+
+#Agendamentos
+
 #getters 
 @app.get("/agendamentos/", response_model=list[Agendamento])
 async def listarAgendamentos():
     query = agendamento.select()
     return await database.fetch_all(query)
 
+@app.get("/agendamentos/{agendamento_id}", response_model=Agendamento)
+async def get_agendamento(agendamento_id: int):
+    query = agendamento.select().where(agendamento.c.id == agendamento_id)
+    return await database.fetch_one(query)
 
-#Post
+@app.put("/agendamentos/{agendamento_id}", response_model=Agendamento)
+async def update_agendamento(agendamento_id: int, agendamento_data: Agendamento):
+    query = agendamento.update().where(agendamento.c.id == agendamento_id).values(
+        nomePaciente=agendamento_data.nome_paciente,
+        descriçao=agendamento_data.descriçao,
+        data=agendamento_data.data_hora,
+    )
+    await database.execute(query)
+    return {**agendamento_data.dict(), "id": agendamento_id}
+
+@app.delete("/agendamentos/{agendamento_id}", response_model=Agendamento)
+async def delete_agendamento(agendamento_id: int):
+    query = agendamento.delete().where(agendamento.c.id == agendamento_id)
+    await database.execute(query)
+    return {"message": "Agendamento deleted successfully", "id": agendamento_id}
+
 
 @app.post("/agendamentos", response_model=Agendamento)
 async def criaAgendamento(agendamentos : Agendamento):
